28bd2112cf683ae7a091d806608a789db8d0e7dd
Eeva Alanko <eeva.alanko@gmail.com>
2016-03-19 21:18:00 +0200

    projektin muokkaus

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="ealanko"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="angtuto"
+PROJECT_FOLDER="ngtuto"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
