5cd5bb31c0feea2152ec73cb79266a1a4eaa61ca
Eeva Alanko <eeva.alanko@gmail.com>
2016-03-25 08:24:08 +0200

    projektia muokattu

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,27 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Usr (id  SERIAL PRIMARY KEY, 
+					email varchar(50) NOT NULL, 
+					name varchar(50) NOT NULL, 
+					password varchar(50) NOT NULL
+					);
+
+CREATE TABLE Favorites (id  SERIAL PRIMARY KEY, 
+						usr_id int4 REFERENCES Usr(id), 
+						tutorial_id REFERENCES Tutorial(id)
+						);
+
+
+CREATE TABLE Tutorial (id  SERIAL PRIMARY KEY, 
+						name varchar(50) NOT NULL, 
+						description varchar(255) NOT NULL, 
+						link varchar(50) NOT NULL, 
+						image bytea NOT NULL, 
+						added date 
+						);
+
+
+CREATE TABLE Review (id  SERIAL PRIMARY KEY, 
+					review varchar(255) NOT NULL, 
+					usr_id int4 REFERENCES Usr(id), 
+					tutorial_id REFERENCES Tutorial(id), 
+					added date 
+					);
